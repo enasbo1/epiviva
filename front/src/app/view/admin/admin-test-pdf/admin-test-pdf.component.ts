9bcad30b49674b98c0f69cfaf3d4b2caa8db44d2
lyess <lyess_boufaden@yahoo.com>
2024-08-27 03:04:14 +0200

    [FEAT][FRONT] changement format pdf

--- front/src/app/view/admin/admin-test-pdf/admin-test-pdf.component.ts
+++ front/src/app/view/admin/admin-test-pdf/admin-test-pdf.component.ts
@@ -11,17 +11,17 @@ export class AdminTestPdfComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    // Toute logique d'initialisation peut aller ici si nécessaire
+    
   }
 
   // Méthode pour générer le PDF
   generatePDF(): void {
     const doc = new jsPDF();  // Créer une instance de jsPDF
     
-    const currentDate = new Date().toLocaleDateString();  // Obtenir la date actuelle au format local
+    const currentDate = new Date().toLocaleDateString();
 
-    doc.text('Date du jour :', 10, 10);  // Ajouter du texte au PDF à la position (10,10)
-    doc.text(currentDate, 10, 20);  // Ajouter la date actuelle en dessous
+    doc.text('Livraison du :', 10, 10);  // Ajouter du texte au PDF à la position (10,10)
+    doc.text(currentDate, 45, 10);  // Ajouter la date actuelle en dessous
 
     doc.save('test_pdf.pdf');  // Sauvegarder le fichier PDF sous le nom "test_pdf.pdf"
   }
